9bf2d65a98661386868af470f8ca89c7093555be
monster1935 <946815480@qq.com>
2023-11-26 09:48:21 +0800

    feat: update

--- binary-tree/104.二叉树的最大深度.js
+++ binary-tree/104.二叉树的最大深度.js
@@ -17,13 +17,35 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function (root) {
-  if (root === null) {
-    return 0;
-  }
-  let depthLeft = maxDepth(root.left);
-  let depthRight = maxDepth(root.right);
 
-  return Math.max(depthLeft, depthRight) + 1;
+// 递归
+// var maxDepth = function(root) {
+//   if (root === null) {
+//     return 0;
+//   }
+//   let depthLeft = maxDepth(root.left);
+//   let depthRight = maxDepth(root.right);
+
+//   return Math.max(depthLeft, depthRight) + 1;
+// };
+var maxDepth = function (root) {
+  let res = 0;
+  let depth = 0;
+
+  const traverse = function (root) {
+    if (root === null) {
+      return;
+    }
+    depth++;
+    if (root.left === null && root.right === null) {
+      res = Math.max(res, depth);
+    }
+    traverse(root.left);
+    traverse(root.right);
+    depth--;
+  };
+
+  traverse(root);
+  return res;
 };
 // @lc code=end
